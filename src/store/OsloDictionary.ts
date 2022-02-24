57ca02f89f426e60a48abeb808b9ba720df11afb
arne.vandenbussche <arne.vandenbussche@student.howest.be>
2022-02-24 15:14:19 +0100

    Fixed bug where duplicates were added

--- src/store/OsloDictionary.ts
+++ src/store/OsloDictionary.ts
@@ -1,19 +1,23 @@
 export function getDictionaryItems() {
-    let Dictionary = JSON.parse(localStorage.getItem("Dictionary"));
-    if(Dictionary == null) Dictionary = []; //if it's empty make a new one
-    return Dictionary
+    let dictionary = JSON.parse(localStorage.getItem("dictionary"));
+    if(dictionary == null) dictionary = []; //if it's empty make a new one
+    return dictionary
 }
 //Gets existing dictionary from storage and adds item
 export function addToDictionary(data: any) {
-    let Dictionary = getDictionaryItems();
-    if(!Dictionary.includes(data.label)){ //FIXME broken
-        Dictionary.push(data);
-        localStorage.setItem("Dictionary", JSON.stringify(Dictionary));
-        document.getElementById("button").innerHTML = data.label+ " is goed toegevoegd!";
-        const myTimeout = setTimeout(changeButtonBack, 2000);
+    let dictionary = getDictionaryItems();
+    let duplicate = false;
+    for (const item of dictionary) {
+        if (item.label === data.label){
+            document.getElementById("button").innerHTML = data.label + " zit al in uw woordenboek";
+            const myTimeout = setTimeout(changeButtonBack, 2000);
+            duplicate = true;
+        }
     }
-    else {
-        document.getElementById("button").innerHTML = data.label + " zit al in uw woordenboek";
+    if(!duplicate){
+        dictionary.push(data);
+        localStorage.setItem("dictionary", JSON.stringify(dictionary));
+        document.getElementById("button").innerHTML = data.label+ " is goed toegevoegd!";
         const myTimeout = setTimeout(changeButtonBack, 2000);
     }
 }
@@ -21,11 +25,16 @@ function changeButtonBack(){
     document.getElementById("button").innerHTML = "Toevoegen aan woordenboek";
 }
 export function deleteFromDictionary(data: any){
-    let Dictionary = getDictionaryItems();
-    let position = Dictionary.indexOf(data.label);
-    Dictionary.splice(position,1);
-    localStorage.setItem("Dictionary", JSON.stringify(Dictionary));
-    location.reload();
-    document.getElementById("button").innerHTML = data.label+ " is verwijderd!";
-    const myTimeout = setTimeout(changeButtonBack, 2000);
+    let dictionary = getDictionaryItems();
+    let i = 0;
+    for (const item of dictionary) {
+        if (item.label === data.label){
+            dictionary.splice(i,1);
+            localStorage.setItem("dictionary", JSON.stringify(dictionary));
+            document.getElementById("button").innerHTML = data.label+ " is verwijderd!";
+            const myTimeout = setTimeout(changeButtonBack, 2000);
+            location.reload();
+        }
+        i++
+    }
 }
