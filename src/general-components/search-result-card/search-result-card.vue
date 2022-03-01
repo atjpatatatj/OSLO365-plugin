42e6497c5ea128c8f10237eece1f39228238c4d3
arne.vandenbussche <arne.vandenbussche@student.howest.be>
2022-03-01 09:05:09 +0100

    Changed phrase for more correct spelling

--- src/general-components/search-result-card/search-result-card.vue
+++ src/general-components/search-result-card/search-result-card.vue
@@ -13,7 +13,7 @@
         :dictionaryItem="dictionaryItem"
         v-if="dictionaryItem === true"
         id="dictionary">
-      <br><br>Zit in uw woordenboek
+      <br><br>Aanwezig in uw woordenboek
     </span>
   </vl-radio-tile>
 </template>
