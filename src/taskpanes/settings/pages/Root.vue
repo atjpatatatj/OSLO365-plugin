c37ac1c828cb933e50578083de59a5de8a2f74d5
arne.vandenbussche <arne.vandenbussche@student.howest.be>
2022-03-07 09:46:43 +0100

    created rough settings layout

--- src/taskpanes/settings/pages/Root.vue
+++ src/taskpanes/settings/pages/Root.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    werkt dit?
+    <vl-layout>
+      <vl-grid v-vl-align:center mod-stacked>
+        <vl-column>
+          <br>
+          <vl-button mod-block @click="deleteEntireDictionary()">Verwijder alle items in woordenboek</vl-button><br>
+          <vl-button mod-block @click="disableDailyDefinition()">Definitie van de dag uitschakkelen</vl-button>
+        </vl-column>
+        <vl-column>
+          mooie grafieken
+        </vl-column>
+      </vl-grid>
+    </vl-layout>
   </div>
 </template>
 
@@ -18,5 +29,6 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+@import "../css/style.scss";
 
 </style>
