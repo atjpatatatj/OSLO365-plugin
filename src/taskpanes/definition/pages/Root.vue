91f5afe36a5c88814b2276e6079b075f78ad2e7b
arne.vandenbussche <arne.vandenbussche@student.howest.be>
2022-03-07 15:18:19 +0100

    Defintion of the day renders

--- src/taskpanes/definition/pages/Root.vue
+++ src/taskpanes/definition/pages/Root.vue
@@ -1,17 +1,37 @@
 <template>
   <div>
-    testtest
+    <vl-layout>
+      <vl-grid v-vl-align:center mod-stacked>
+        <vl-column>
+          interessante definitie van de dag
+        </vl-column>
+        <vl-column>
+          {{ randomDefinition.label }}
+        </vl-column>
+        <vl-column>
+          {{ randomDefinition.description }}
+        </vl-column>
+        <vl-column>
+          {{ randomDefinition.reference }}
+        </vl-column>
+      </vl-grid>
+    </vl-layout>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import {deleteEntireDictionary} from "../../../store/OsloDictionary";
+import {OsloStore} from "../../../store/OsloStore";
+
+const osloStore = OsloStore.getInstance();
+const definition = osloStore.getRandomDefinition();
 
 export default Vue.extend({
   name: "root",
   data: () => {
     return {
+      randomDefinition: definition
     };
   }
 });
