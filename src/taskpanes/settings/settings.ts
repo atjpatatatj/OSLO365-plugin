aca7dac023b7438c330fb2a4d21940d34d7498a3
arne.vandenbussche <arne.vandenbussche@student.howest.be>
2022-03-08 10:53:52 +0100

    Changed settings to taskpane due to a lot of issues with the dialog API

--- src/taskpanes/settings/settings.ts
+++ src/taskpanes/settings/settings.ts
@@ -4,6 +4,9 @@ import Vue from "vue";
 import Vuex from "vuex";
 import root from "./pages/Root.vue";
 const VlUiVueComponents = require("@govflanders/vl-ui-vue-components");
+/*
+Due to a lot of issues with the diagram API it's currently set as a taskpane
+ */
 
 /// configuration of the built-in validator
 const validatorConfig = {
@@ -24,17 +27,20 @@ Office.onReady((info) => {
         });
     }
 });
-function enableDefintionOTD(){
+export function enableDefintionOTD(){
     console.log('Current value for mySetting: ' + Office.context.document.settings.get('Office.AutoShowTaskpaneWithDocument'));
     Office.context.document.settings.set("Office.AutoShowTaskpaneWithDocument", true);
     Office.context.document.settings.saveAsync();
     console.log('set');
     console.log('Current value for mySetting: ' + Office.context.document.settings.get('Office.AutoShowTaskpaneWithDocument'));
 }
-function disableDefinitionOTD(){
+export function disableDefinitionOTD(){
     console.log('Current value for mySetting: ' + Office.context.document.settings.get('Office.AutoShowTaskpaneWithDocument'));
     Office.context.document.settings.remove("Office.AutoShowTaskpaneWithDocument");
     Office.context.document.settings.saveAsync();
     console.log('removed');
     console.log('Current value for mySetting: ' + Office.context.document.settings.get('Office.AutoShowTaskpaneWithDocument'));
+}
+export function defintionOTDSetting(){
+    return Office.context.document.settings.get('Office.AutoShowTaskpaneWithDocument');
 }
