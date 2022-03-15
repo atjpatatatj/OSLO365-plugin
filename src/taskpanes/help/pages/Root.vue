0afd9cf976104a06b83fc1cf0840c3e1842120c6
arne.vandenbussche <arne.vandenbussche@student.howest.be>
2022-03-15 11:36:07 +0100

    basic layout for help dialog

--- src/taskpanes/help/pages/Root.vue
+++ src/taskpanes/help/pages/Root.vue
@@ -2,19 +2,32 @@
   <div>
     <div data-vl-tabs data-vl-tabs-responsive-label="Navigatie">
       <div class="vl-tabs__wrapper">
-        <div class="vl-tabs " data-vl-tabs-list role="tablist"><a class="vl-tab vl-tab--active" id="vl-tab-nummer-1" role="tab" data-vl-tab aria-controls="vl-tab-nummer-1-pane" href="#vl-tab-nummer-1">
-          Trein
+        <div class="vl-tabs " data-vl-tabs-list role="tablist"><a class="vl-tab vl" id="vl-tab-nummer-1" role="tab" data-vl-tab aria-controls="vl-tab-nummer-1-pane" href="#vl-tab-nummer-1">
+          Zoeken
         </a><a class="vl-tab" id="vl-tab-nummer-2" role="tab" data-vl-tab aria-controls="vl-tab-nummer-2-pane" href="#vl-tab-nummer-2">
-          Metro, tram en bus
+          Selectie zoeken
         </a><a class="vl-tab" id="vl-tab-nummer-3" role="tab" data-vl-tab aria-controls="vl-tab-nummer-3-pane" href="#vl-tab-nummer-3">
-          Fiets
+          Voetnoot
+        </a><a class="vl-tab" id="vl-tab-nummer-4" role="tab" data-vl-tab aria-controls="vl-tab-nummer-4-pane" href="#vl-tab-nummer-4">
+          Eindnoot
+        </a><a class="vl-tab" id="vl-tab-nummer-5" role="tab" data-vl-tab aria-controls="vl-tab-nummer-5-pane" href="#vl-tab-nummer-5">
+          Document scan
+        </a><a class="vl-tab" id="vl-tab-nummer-6" role="tab" data-vl-tab aria-controls="vl-tab-nummer-6-pane" href="#vl-tab-nummer-6">
+          Toevoegen aan woordenboek
+        </a><a class="vl-tab" id="vl-tab-nummer-7" role="tab" data-vl-tab aria-controls="vl-tab-nummer-7-pane" href="#vl-tab-nummer-7">
+          Verwijderen van woordenboek
+        </a><a class="vl-tab" id="vl-tab-nummer-8" role="tab" data-vl-tab aria-controls="vl-tab-nummer-8-pane" href="#vl-tab-nummer-8">
+          Definitie van de dag
+        </a><a class="vl-tab" id="vl-tab-nummer-9" role="tab" data-vl-tab aria-controls="vl-tab-nummer-9-pane" href="#vl-tab-nummer-9">
+          Uw top 5 definities
         </a>
+
         </div>
         <button type="button" data-vl-tabs-toggle aria-expanded="false" class="vl-tabs__toggle" data-vl-close="false"><span>Navigatie</span></button>
       </div>
       <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-1-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-1" data-vl-show="true">
         <div class="vl-title-wrapper vl-title-wrapper--cta">
-          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Trein</h3>
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Zoeken</h3>
         </div>
         <div class="vl-typography">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -22,7 +35,7 @@
       </section>
       <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-2-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-2">
         <div class="vl-title-wrapper vl-title-wrapper--cta">
-          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Metro, tram en bus</h3>
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Selectie zoeken</h3>
         </div>
         <div class="vl-typography">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -30,7 +43,55 @@
       </section>
       <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-3-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-3">
         <div class="vl-title-wrapper vl-title-wrapper--cta">
-          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Fiets</h3>
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Voetnoot</h3>
+        </div>
+        <div class="vl-typography">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+      <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-4-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-4">
+        <div class="vl-title-wrapper vl-title-wrapper--cta">
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Eindnoot</h3>
+        </div>
+        <div class="vl-typography">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+      <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-5-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-5">
+        <div class="vl-title-wrapper vl-title-wrapper--cta">
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Document scan</h3>
+        </div>
+        <div class="vl-typography">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+      <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-6-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-6">
+        <div class="vl-title-wrapper vl-title-wrapper--cta">
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Toevoegen aan woordenboek</h3>
+        </div>
+        <div class="vl-typography">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+      <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-7-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-7">
+        <div class="vl-title-wrapper vl-title-wrapper--cta">
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Verwijderen van woordenboek</h3>
+        </div>
+        <div class="vl-typography">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+      <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-8-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-8">
+        <div class="vl-title-wrapper vl-title-wrapper--cta">
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Verwijderen van woordenboek</h3>
+        </div>
+        <div class="vl-typography">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+      <section class="vl-col--1-1 vl-tab__pane" data-vl-tab-pane tabindex="0" role="tabpanel" id="vl-tab-nummer-9-pane" hidden="hidden" aria-labelledby="vl-tab-nummer-9">
+        <div class="vl-title-wrapper vl-title-wrapper--cta">
+          <h3 class="vl-title vl-title--h3 vl-title-wrapper__title vl-title-wrapper--cta__title">Uw top 5 definities</h3>
         </div>
         <div class="vl-typography">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -41,11 +102,13 @@
 </template>
 
 <script lang="ts">
+import { Tabs } from '@govflanders/vl-ui-tabs';
 import Vue from "vue";
 export default Vue.extend({
   name: "root",
   data: () => {
     return {
+      page : 0
     };
   },
 });
@@ -54,4 +117,10 @@ export default Vue.extend({
 <style lang="scss">
 @import "../css/style.scss";
 @import "~@govflanders/vl-ui-tabs/src/scss/tabs";
+a:visited{
+  color: #05c;
+}
+.vl-tab__pane{
+  padding-left: 10px;
+}
 </style>
